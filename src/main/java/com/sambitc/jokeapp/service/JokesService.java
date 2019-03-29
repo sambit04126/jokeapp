f3d52136861da185b06512e9d58a45afd6ef36c1
sambitc <sambit04126@gmail.com>
2019-03-29 08:02:43 +0530

    Changes done after adding java configuration

--- src/main/java/com/sambitc/jokeapp/service/JokesService.java
+++ src/main/java/com/sambitc/jokeapp/service/JokesService.java
@@ -1,5 +1,6 @@
 package com.sambitc.jokeapp.service;
 
+import com.sambitc.jokeapp.JokeappApplication;
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -8,10 +9,16 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 @Service
-public class JokesService extends ChuckNorrisQuotes{
+public class JokesService {
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokesService(ChuckNorrisQuotes chuckNorrisQuotes){
+        this.chuckNorrisQuotes=chuckNorrisQuotes;
+    }
 
     public String getJoke(){
-        String joke=this.getRandomQuote();
+        String joke=chuckNorrisQuotes.getRandomQuote();
         return joke;
     }
 }
